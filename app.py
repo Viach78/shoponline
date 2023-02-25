c36d0da3de27f431f0950fef95b4210cc0395af9
Slava <ipvorobyev@mail.ru>
2023-02-25 11:50:48 +0300

    db_shop_price

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, current_app
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
@@ -12,6 +12,9 @@ class Item(db.Model):
     title = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Integer, nullable=False)
     isActive = db.Column(db.Boolean, default=True)
+
+    def __repr__(self):
+        return f'Запись:{self.title}'
 
 
 with app.app_context():
@@ -22,7 +25,8 @@ with app.app_context():
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    item = Item.query.order_by(Item.price).all()
+    return render_template("index.html", item=item)
 
 
 @app.route('/about')
@@ -30,9 +34,23 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/create')
+@app.route('/create', methods=['POST', 'GET'])
 def create():
-    return render_template("create.html")
+    if request.method == 'POST':
+        title = request.form['title']
+        price = request.form['price']
+
+        item = Item(title=title, price=price)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Ошибка ввода данных"
+    else:
+
+        return render_template("create.html")
 
 
 
